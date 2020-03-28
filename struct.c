cb70fc55ae006394a708830998303a331cc195ce
mohamed_ayad <55093384+mhayad236@users.noreply.github.com>
2020-03-28 18:15:05 +0200

    Create struct.c

---
+++ struct.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+struct human {
+   char  name[50];
+   char  job[50];
+   int  age;
+};
+
+int main( ) {
+
+   struct human ayad;        /* Declare Book1 of type Book */
+   struct human hero;        /* Declare Book2 of type Book */
+
+   /* book 1 specification */
+   strcpy( ayad.name, "mhayad");
+   strcpy( ayad.job, "pentester");
+   ayad.age = 18;
+
+   /* book 2 specification */
+   strcpy( hero.name, "heroooo");
+   strcpy( hero.job, "Cyber-Security_Researcher");
+   hero.age = 20;
+
+   /* print Book1 info */
+   printf( "first human name is  : %s\n", ayad.name);
+   printf( "first human job is   :  %s\n", ayad.job);
+   printf( "first human age is   :  %d\n", ayad.age);
+
+   /* print Book2 info */
+   printf( "second human name is : %s\n", hero.name);
+   printf( "second human job is  :  %s\n", hero.job);
+   printf( "second human age is  :  %d\n", hero.age);
+
+   return 0;
+}
